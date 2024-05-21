2f485045b56339b5dea053b83777c7cec60ded05
David Taylor <davidtaylor@student.midlandstech.edu>
2024-05-21 18:27:11 -0400

    final commit

---
+++ Selfies/frmSelfies.Designer.vb
@@ -0,0 +1,156 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmSelfies
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblTitle = New System.Windows.Forms.Label()
+        Me.btnCheer = New System.Windows.Forms.Button()
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.btnSelect = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lblChoose = New System.Windows.Forms.Label()
+        Me.lblThanks = New System.Windows.Forms.Label()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblTitle
+        '
+        Me.lblTitle.AutoSize = True
+        Me.lblTitle.Font = New System.Drawing.Font("Tahoma", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTitle.Location = New System.Drawing.Point(37, 13)
+        Me.lblTitle.Name = "lblTitle"
+        Me.lblTitle.Size = New System.Drawing.Size(341, 25)
+        Me.lblTitle.TabIndex = 0
+        Me.lblTitle.Text = "Vote for the Best Campus Selfie"
+        '
+        'btnCheer
+        '
+        Me.btnCheer.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCheer.Location = New System.Drawing.Point(42, 53)
+        Me.btnCheer.Name = "btnCheer"
+        Me.btnCheer.Size = New System.Drawing.Size(120, 30)
+        Me.btnCheer.TabIndex = 1
+        Me.btnCheer.Text = "Finalist ""Cheer"""
+        Me.btnCheer.UseVisualStyleBackColor = True
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnConnect.Location = New System.Drawing.Point(258, 53)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(120, 30)
+        Me.btnConnect.TabIndex = 2
+        Me.btnConnect.Text = "Finalist ""Connect"""
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 89)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(185, 150)
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Location = New System.Drawing.Point(217, 89)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(185, 150)
+        Me.PictureBox2.TabIndex = 4
+        Me.PictureBox2.TabStop = False
+        '
+        'btnSelect
+        '
+        Me.btnSelect.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSelect.Location = New System.Drawing.Point(155, 245)
+        Me.btnSelect.Name = "btnSelect"
+        Me.btnSelect.Size = New System.Drawing.Size(107, 23)
+        Me.btnSelect.TabIndex = 5
+        Me.btnSelect.Text = "Select Selfie"
+        Me.btnSelect.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnExit.Location = New System.Drawing.Point(171, 310)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 6
+        Me.btnExit.Text = "Exit Window"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lblChoose
+        '
+        Me.lblChoose.AutoSize = True
+        Me.lblChoose.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblChoose.Location = New System.Drawing.Point(39, 271)
+        Me.lblChoose.Name = "lblChoose"
+        Me.lblChoose.Size = New System.Drawing.Size(345, 14)
+        Me.lblChoose.TabIndex = 7
+        Me.lblChoose.Text = "Choose the best selfie and then click the Select Selfie button"
+        '
+        'lblThanks
+        '
+        Me.lblThanks.AutoSize = True
+        Me.lblThanks.Font = New System.Drawing.Font("Tahoma", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblThanks.Location = New System.Drawing.Point(152, 293)
+        Me.lblThanks.Name = "lblThanks"
+        Me.lblThanks.Size = New System.Drawing.Size(103, 14)
+        Me.lblThanks.TabIndex = 8
+        Me.lblThanks.Text = "Thanks for voting"
+        '
+        'frmSelfies
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(414, 356)
+        Me.Controls.Add(Me.lblThanks)
+        Me.Controls.Add(Me.lblChoose)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSelect)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Controls.Add(Me.btnCheer)
+        Me.Controls.Add(Me.lblTitle)
+        Me.Name = "frmSelfies"
+        Me.Text = "Selfies"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblTitle As Label
+    Friend WithEvents btnCheer As Button
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents PictureBox2 As PictureBox
+    Friend WithEvents btnSelect As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents lblChoose As Label
+    Friend WithEvents lblThanks As Label
+End Class
---
+++ Selfies/frmSelfies.vb
@@ -0,0 +1,3 @@
+﻿Public Class frmSelfies
+
+End Class
